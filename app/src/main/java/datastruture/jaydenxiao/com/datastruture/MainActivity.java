40e7bae3b382680eed23369f5249bbd902c14e06
xsf <jaydenxiao2016@gmail.com>
2018-12-09 14:43:22 +0800

    Initial commit

---
+++ app/src/main/java/datastruture/jaydenxiao/com/datastruture/MainActivity.java
@@ -0,0 +1,13 @@
+package datastruture.jaydenxiao.com.datastruture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
